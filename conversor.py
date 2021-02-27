88dcf4a7f837532e543c2db309a6375f95c5395a
Alex Viana <alexgviana7@gmail.com>
2021-02-26 20:12:31 -0600

    Conversor de usd to sv / sv to usd

--- conversor.py
+++ conversor.py
@@ -1,7 +1,15 @@
-pesos = input("¿Cuántos pesos colombianos tienes? ")
-pesos = float(pesos)
-valor_dolar=3647
-dolares= pesos / valor_dolar
+colones = input("¿Cuántos colones salvadoreños tienes? ")
+colones = float(colones)
+valor_dolar=8.75
+dolares= colones / valor_dolar
 dolares = round(dolares,2)
 dolares= str(dolares)
 print("Tienes $" + dolares + " dólares" )
+
+dolares = input("¿Cuántos USD tienes? ")
+dolares=float(dolares)
+valor_colon=0.11
+colones = dolares / valor_colon
+colones = round(colones,2)
+colones=str(colones)
+print("Tienes ¢ " + colones + " colones")
