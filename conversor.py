3a3938b14eef183fabd1c4c0496574de7f4ddc38
Alex Viana <alexgviana7@gmail.com>
2021-02-26 21:18:01 -0600

    Conversor de 3 monedas con código repetitivo

--- conversor.py
+++ conversor.py
@@ -1,15 +1,38 @@
-colones = input("¿Cuántos colones salvadoreños tienes? ")
-colones = float(colones)
-valor_dolar=8.75
-dolares= colones / valor_dolar
-dolares = round(dolares,2)
-dolares= str(dolares)
-print("Tienes $" + dolares + " dólares" )
+menu = """
+Bienvenido al conversor de monedas 💰
 
-dolares = input("¿Cuántos USD tienes? ")
-dolares=float(dolares)
-valor_colon=0.11
-colones = dolares / valor_colon
-colones = round(colones,2)
-colones=str(colones)
-print("Tienes ¢ " + colones + " colones")
+1- Pesos colombianos
+2- Pesos argentinos
+3- Pesos mexicanos
+
+Elige una opción:   """
+
+opcion = int(input(menu))
+
+if opcion== 1:
+    pesos= input("¿Cuántos pesos colombianos tienes ")
+    pesos= float(pesos)
+    valor_dolar=3647
+    dolares= pesos / valor_dolar
+    dolares= round(dolares,2)
+    dolares=str(dolares)
+    print("Tienes $" + dolares + " dólares")
+
+elif opcion== 2:
+    pesos= input("¿Cuántos pesos argentinos tienes ")
+    pesos= float(pesos)
+    valor_dolar=89.95
+    dolares= pesos / valor_dolar
+    dolares= round(dolares,2)
+    dolares=str(dolares)
+    print("Tienes $" + dolares + " dólares")    
+elif opcion== 3:
+    pesos= input("¿Cuántos pesos mexicanos tienes ")
+    pesos= float(pesos)
+    valor_dolar=20.86
+    dolares= pesos / valor_dolar
+    dolares= round(dolares,2)
+    dolares=str(dolares)
+    print("Tienes $" + dolares + " dólares")
+else:
+    print("Ingresa una opción correcta")
