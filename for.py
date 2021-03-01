3a2b89306257049f56b3539d029d386262122619
Alex Viana <alexgviana7@gmail.com>
2021-02-28 19:12:45 -0600

    Implementando el ciclo for

---
+++ for.py
@@ -0,0 +1,16 @@
+# contador=1
+# print(contador)
+# while contador < 1000:
+#     contador+=1
+#     print(contador)
+
+
+# for contador in range(1, 1001):
+#     print(contador)
+
+#Ciclo for para imprimir la tabla del 11
+
+for i in range (10):
+    print(11 * i)
+
+
