a0b0ef481cd18aad4ece9a85c3177a1864606dd7
Alex Viana <alexgviana7@gmail.com>
2021-02-28 19:49:32 -0600

    Utilizando continue y break. Se añade el desafío

---
+++ break_continue.py
@@ -0,0 +1,32 @@
+def run():
+    # for contador in range (1000):
+    #     if contador % 2 != 0:
+    #         continue
+    #     print(contador)
+
+    # for i in range (10000):
+    #     print(i)
+    #     if i == 5678:
+    #         break
+
+    # texto = input("Escribe un texto: ")
+    # for letra in texto:
+    #     if letra == "o":
+    #         break
+    #     print(letra)
+
+
+#Desafio
+
+    contador=1
+    while contador < 500:
+        print(contador)
+        contador+=1
+        if contador == 75:
+            break
+    
+
+      
+
+if __name__ == "__main__":
+    run()
